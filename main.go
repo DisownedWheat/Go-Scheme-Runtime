ba2d74bf7764f56d9873cfb12e512e7276033da1
Rob <robert.larnach@gmail.com>
2017-09-10 22:52:57 +1000

    Fix how environments are structured. Make calls and gets global function with environments passed to them. Add a bit to the runtime so it actually does something now.

--- main.go
+++ main.go
@@ -1,46 +1,153 @@
-package goScheme
+package RT
 
 type Env struct {
 	Variables map[string]SchemeInterface
-	CallStack []func(args ...SchemeInterface) SchemeInterface
-	Closures  map[string]*Env
-	RootEnv   *Env
+	Functions map[string]SchemeFunc
 	Parent    *Env
-	Name      string
+	type_     string
 }
 
-func (e *Env) Call(funcName string) {
-	if nextEnv, ok := e.Closures[funcName]; ok {
-		e.Closures[nextEnv.Name] = nextEnv
-	} else {
-		if e.Parent == nil {
-			panic("NO FUNCTION BY THAT NAME")
+func (e *Env) Init(p *Env) Env {
+	e.Variables = map[string]SchemeInterface{}
+	e.Functions = map[string]SchemeFunc{}
+	e.Parent = p
+	e.type_ = "Env"
+	return *e
+}
+
+func (e *Env) GetType() string {
+	return e.type_
+}
+
+func Call(e *Env, originalEnv *Env, funcName string, args []SchemeInterface) SchemeInterface {
+	funcError := "NO FUNCTION BY The Name " + funcName
+	if i, ok := e.Variables[funcName]; ok {
+
+		f, ok := i.(SchemeFunc)
+		if !ok {
+			if e.Parent == nil {
+				panic("This is not a function")
+			}
+			return Call(e.Parent, originalEnv, funcName, args)
 		}
-		e.Parent.Call(funcName)
+		return f.Value(originalEnv, args)
 	}
+
+	if e.Parent == nil {
+		panic(funcError)
+	}
+	return Call(e.Parent, originalEnv, funcName, args)
 }
 
-type SchemeType struct {
-	type_ string
+func Get(e *Env, originalEnv *Env, varName string) SchemeInterface {
+	varError := "Undefined variable: " + varName
+	if i, ok := e.Variables[varName]; ok {
+		v, ok := i.(SchemeInterface)
+		if !ok {
+			if e.Parent == nil {
+				panic(varError)
+			}
+			return Get(e.Parent, e, varName)
+		}
+		return v
+	}
+
+	if e.Parent == nil {
+		panic(varError)
+	}
+	return Get(e.Parent, e, varName)
 }
 
-func (s *SchemeType) GetType() string {
-	return s.type_
+type SchemeString struct {
+	Value string
 }
+
+func (s SchemeString) GetType() string       { return "String" }
+func (s SchemeString) GetValue() interface{} { return s.Value }
+
+type SchemeNumber struct {
+	Value int
+}
+
+func (s SchemeNumber) GetType() string       { return "Number" }
+func (s SchemeNumber) GetValue() interface{} { return s.Value }
+
+type SchemeFunc struct {
+	Value func(e *Env, args []SchemeInterface) SchemeInterface
+	Env   *Env
+}
+
+func (sf SchemeFunc) GetType() string {
+	return "Function"
+}
+func (s SchemeFunc) GetValue() interface{} { return nil }
+
+type SchemeSymbol struct {
+	Value string
+}
+
+func (s SchemeSymbol) GetType() string       { return "Symbol" }
+func (s SchemeSymbol) GetValue() interface{} { return s.Value }
 
 type SchemeInterface interface {
 	GetType() string
+	GetValue() interface{} // This is for debugging purposes
 }
 
 func main() {}
 
-func MakeRootEnv() {
-	root := Env{}
+func MakeRootEnv() *Env {
+	var root Env
+	root.Init(nil)
 
-	rootEnv.closures["define"] = func(e *Env, name SchemeType, value SchemeInterface) {
-		type_ = value.GetType()
-		if type_ == "CallExpression" {
-			e.closures[name.(string)] = value
-		}
+	root.Variables["def"] = SchemeFunc{
+		Value: func(e *Env, args []SchemeInterface) SchemeInterface {
+			name := args[0].(SchemeSymbol)
+			value := args[1].(SchemeInterface)
+			e.Variables[name.Value] = value
+			return value
+		},
 	}
+
+	root.Variables["+"] = SchemeFunc{
+		Value: func(e *Env, args []SchemeInterface) SchemeInterface {
+			total := 0
+			for _, arg := range args {
+				switch v := arg.(type) {
+				case SchemeNumber:
+					total += v.Value
+				case SchemeSymbol:
+					x := Get(e, e, v.Value)
+					val, ok := x.(SchemeNumber)
+					if !ok {
+						panic("Cannot add this type")
+					}
+					total += val.Value
+				default:
+					panic("Cannot add this type")
+				}
+			}
+			return SchemeNumber{Value: total}
+		},
+	}
+
+	root.Variables["print"] = SchemeFunc{
+		Value: func(e *Env, args []SchemeInterface) SchemeInterface {
+			for _, arg := range args {
+				switch v := arg.(type) {
+				case SchemeNumber:
+					print(v.Value, "")
+				case SchemeString:
+					print(v.Value, "")
+				case SchemeFunc:
+					print("Function", "")
+				default:
+					print(v.GetValue(), " ")
+				}
+			}
+			return SchemeNumber{Value: 0}
+		},
+	}
+
+	return &root
 }
